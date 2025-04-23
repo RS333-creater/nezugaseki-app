ae5ec063acb20abc804093f97cd079ee9b884273
samurai <150229185+RS333-creater@users.noreply.github.com>
2025-04-23 18:15:16 +0900

    new

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+// App.jsx
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Multifunction from "./pages/Multifunction";
+import DayService from "./pages/DayService";
+import Pricing from "./pages/Pricing";
+import Access from "./pages/Access";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/facility/multifunction" element={<Multifunction />} />
+        <Route path="/facility/day-service" element={<DayService />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/access" element={<Access />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
